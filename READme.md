14cccd43a1110d7e7140d23ac6350d0da84a24d9
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-02-01 12:37:54 -0300

    Alura Books com Arrays

---
+++ READme.md
@@ -0,0 +1,9 @@
+### JAVASCRIPT MÉTODOS ARRAY
+
+Curso administrado pelo professor Guilherme Lima.
+
+No curso é abordado sobre Arrays entre eles: Map, Reduce e Filter.
+
+O projeto final em si é mostrar a forma que se filtra produtos no site Alura Books.
+
+Curso finalizado em: 01/02/2023.
---
+++ apps/metodoSort.js
@@ -0,0 +1,8 @@
+let btnOdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco() {
+    let lirvosOrdenados = livros.sort((a, b) => a.preco - b.preco);
+    
+    exibirOsLivrosNaTela(lirvosOrdenados);
+}
